c1d84ef752bb6c3ef62e38c256b4162fe75ea6dd
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-27 17:59:59 +0300

    Add Mongoose models: User, Food, Reservation

---
+++ server/models/Reservation.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const reservationSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    foodItem: { type: mongoose.Schema.Types.ObjectId, ref: "Food" },
+    date: String,
+    time: String,
+    creditCard: {
+      cardNumber: String,
+      cardHolder: String,
+      expirationDate: String,
+      cvv: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Reservation", reservationSchema);
