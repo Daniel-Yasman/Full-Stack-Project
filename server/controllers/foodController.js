0d5b76d5fa4b2eac0d83efbe5621648ab6a7746e
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-28 17:02:11 +0300

    Add controller logic to list food items

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -1,6 +1,11 @@
+const Food = require("../models/Food");
+
 const listFoodItems = async (req, res) => {
   try {
-    console.log("food endpoint hit");
+    const foods = await Food.find({});
+    if (foods.length === 0)
+      return res.status(404).json({ message: "No Foods found" });
+    return res.status(200).json({ message: "Success", foods });
   } catch (error) {
     console.error(error.message);
     return res
