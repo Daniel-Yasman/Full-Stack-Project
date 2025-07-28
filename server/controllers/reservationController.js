0b3b1aea8510ef1ab8061a419026716c348bb624
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-28 18:10:03 +0300

    Add controller logic to create and list reservations

--- server/controllers/reservationController.js
+++ server/controllers/reservationController.js
@@ -1,6 +1,23 @@
+const Reservation = require("../models/Reservation");
+
 const createReservation = async (req, res) => {
+  const { userId, foodItem, date, time, creditCard } = req.body;
+  if (!userId || !foodItem || !date || !time || !creditCard)
+    return res.status(400).json({ message: "Missing fields" });
+
   try {
-    console.log("reservations endpoint hit");
+    const newReservation = new Reservation({
+      userId,
+      foodItem,
+      date,
+      time,
+      creditCard,
+    });
+    await newReservation.save();
+    return res.status(200).json({
+      message: "Reservation created successfully",
+      reservation: newReservation,
+    });
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({
@@ -11,11 +28,16 @@ const createReservation = async (req, res) => {
 
 const listReservations = async (req, res) => {
   try {
-    console.log("history endpoint hit");
+    const reservations = await Reservation.find({});
+
+    if (reservations.length === 0)
+      return res.status(404).json({ message: "No reservations found" });
+
+    return res.status(200).json({ message: "Success", reservations });
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({
-      message: "An error occurred while trying to create get reservasions",
+      message: "An error occurred while trying to get reservations",
     });
   }
 };
