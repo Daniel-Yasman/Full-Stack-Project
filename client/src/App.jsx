19a611f7c7def3c9d28d75b587d90050fd1ebc35
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-29 21:36:34 +0300

    Clean up code formatting and structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import FoodList from "./components/FoodList";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    // <Register />
-    // <Login />
-    <FoodList />
+    <div>Home page</div>
     // <>
     //   <div>
     //     <a href="https://vite.dev" target="_blank">
