7ae69766288dbc916fc67de9e5df8b04d471053d
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-29 21:35:37 +0300

    Create Reservation page with form and submission logic

---
+++ client/src/pages/Reservation.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+function Reservation() {
+  const location = useLocation();
+  /*
+  in order to create reservation I need
+  • userId
+  • foodId
+  • date
+  • time
+  • creditCard:{cardNumber,cardHolder,expirationDate,cvv}
+  I get sent the foodId on button click, userId is in localStorage
+  now all I need is to make it work.
+    */
+  const userId = JSON.parse(localStorage.getItem("user"))._id;
+  const foodId = location.state.foodId;
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardHolder, setCardHolder] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/reservations/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId,
+        foodId,
+        date,
+        time,
+        creditCard: {
+          cardNumber,
+          cardHolder,
+          expirationDate,
+          cvv,
+        },
+      }),
+    });
+    if (!response.ok) {
+      let errorData;
+      try {
+        errorData = await response.json();
+      } catch {
+        errorData = { message: "Unknown error" };
+      }
+      setMessage(`${errorData.message}.`);
+      return;
+    }
+    setMessage(
+      JSON.stringify({
+        userId,
+        foodId,
+        date,
+        time,
+        creditCard: {
+          cardNumber,
+          cardHolder,
+          expirationDate,
+          cvv,
+        },
+      })
+    );
+  };
+  return (
+    <div>
+      {message && <p>{message}</p>}
+      {foodId && <p>{foodId}</p>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Date & Time</legend>
+          <label>Date:</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <label>Time:</label>
+          <input
+            type="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Payment Info</legend>
+          <label>Card Number:</label>
+          <input
+            type="text"
+            value={cardNumber}
+            onChange={(e) => setCardNumber(e.target.value)}
+          />
+          <label>Card Holder:</label>
+          <input
+            type="text"
+            value={cardHolder}
+            onChange={(e) => setCardHolder(e.target.value)}
+          />
+          <label>Expiry:</label>
+          <input
+            type="text"
+            value={expirationDate}
+            onChange={(e) => setExpirationDate(e.target.value)}
+          />
+          <label>CVV</label>
+          <input
+            type="text"
+            value={cvv}
+            onChange={(e) => setCvv(e.target.value)}
+          />
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+export default Reservation;
