92b338a0fa85b5008bdc498be6885f9adf7b20b2
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-29 21:34:29 +0300

    Set up React Router with routes for register, login, menu, and reservation pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import FoodList from "./components/FoodList";
+import Reservation from "./pages/reservation.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/menu" element={<FoodList />} />
+        <Route path="/reserve" element={<Reservation />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
