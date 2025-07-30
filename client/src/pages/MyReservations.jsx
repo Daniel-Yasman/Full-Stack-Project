c8596fc192ddfa015f7550c21732c14d9e483dab
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-30 16:03:15 +0300

    Create MyReservations page with user greeting and reservation list

---
+++ client/src/pages/MyReservations.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+function MyReservations() {
+  // call fetch add users id via localstorage
+  const userId = JSON.parse(localStorage.getItem("user"))._id;
+  const name = JSON.parse(localStorage.getItem("user")).name;
+  const [reservations, setReservations] = useState([]);
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const fetchReservations = async () => {
+      const response = await fetch(`/api/reservations?userId=${userId}`);
+      if (!response.ok) {
+        let errorData;
+        try {
+          errorData = response.json();
+        } catch {
+          errorData = { message: "Unknown error" };
+        }
+        setMessage(errorData);
+      }
+      setMessage("Success");
+      const data = await response.json();
+      setReservations(data.reservations);
+    };
+
+    fetchReservations();
+  }, []);
+
+  return (
+    <div>
+      {userId && <p>{userId}</p>}
+      {message && <p>{message}</p>}
+      <h1>HUllu {name && <span>{name}</span>} welcom 2 home p4g3!</h1>
+      <div>
+        <p>Heree histori:</p>
+        {reservations.length === 0 ? (
+          <p>No reservations yet</p>
+        ) : (
+          reservations.map((reservation) => (
+            <div key={reservation._id}>
+              <div>{reservation.date}</div>
+              <div>{reservation.time}</div>
+              <div>{reservation.foodId.name}</div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+export default MyReservations;
