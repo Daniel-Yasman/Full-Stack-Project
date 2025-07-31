c93d265dd09a3d4eac07ff1e665106c5695608bc
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-31 17:41:26 +0300

    Add reservation form with submission logic and success modal integration

--- client/src/pages/Reservation.jsx
+++ client/src/pages/Reservation.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useLocation, Link } from "react-router-dom";
+import SuccessModal from "../components/SuccessModal";
 function Reservation() {
   const location = useLocation();
   /*
@@ -55,79 +56,72 @@ function Reservation() {
       setMessage(`${errorData.message}.`);
       return;
     }
-    setMessage(
-      JSON.stringify({
-        userId,
-        foodId: food._id,
-        date,
-        time,
-        creditCard: {
-          cardNumber,
-          cardHolder,
-          expirationDate,
-          cvv,
-        },
-      })
-    );
+    setMessage("Success");
   };
   return (
     <div>
-      {userId ? (
-        <div>
-          <Link to="/">Home</Link>
-          {message && <p>{message}</p>}
-          {name && <p>{name}</p>}
-          {food?.name && <p>{food.name}</p>}
-          <form onSubmit={handleSubmit}>
-            <fieldset>
-              <legend>Date & Time</legend>
-              <label>Date:</label>
-              <input
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-              />
-              <label>Time:</label>
-              <input
-                type="time"
-                value={time}
-                onChange={(e) => setTime(e.target.value)}
-              />
-            </fieldset>
-            <fieldset>
-              <legend>Payment Info</legend>
-              <label>Card Number:</label>
-              <input
-                type="text"
-                value={cardNumber}
-                onChange={(e) => setCardNumber(e.target.value)}
-              />
-              <label>Card Holder:</label>
-              <input
-                type="text"
-                value={cardHolder}
-                onChange={(e) => setCardHolder(e.target.value)}
-              />
-              <label>Expiry:</label>
-              <input
-                type="text"
-                value={expirationDate}
-                onChange={(e) => setExpirationDate(e.target.value)}
-              />
-              <label>CVV</label>
-              <input
-                type="text"
-                value={cvv}
-                onChange={(e) => setCvv(e.target.value)}
-              />
-            </fieldset>
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+      {message === "Success" ? (
+        <SuccessModal name={name} food={food.name} date={date} time={time} />
       ) : (
         <div>
-          <Link to="/">Home</Link>
-          <p>Log in first</p>
+          {userId ? (
+            <div>
+              <Link to="/">Home</Link>
+              {message && <p>{message}</p>}
+              {name && <p>{name}</p>}
+              {food?.name && <p>{food.name}</p>}
+              <form onSubmit={handleSubmit}>
+                <fieldset>
+                  <legend>Date & Time</legend>
+                  <label>Date:</label>
+                  <input
+                    type="date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                  />
+                  <label>Time:</label>
+                  <input
+                    type="time"
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset>
+                  <legend>Payment Info</legend>
+                  <label>Card Number:</label>
+                  <input
+                    type="text"
+                    value={cardNumber}
+                    onChange={(e) => setCardNumber(e.target.value)}
+                  />
+                  <label>Card Holder:</label>
+                  <input
+                    type="text"
+                    value={cardHolder}
+                    onChange={(e) => setCardHolder(e.target.value)}
+                  />
+                  <label>Expiry:</label>
+                  <input
+                    type="text"
+                    value={expirationDate}
+                    onChange={(e) => setExpirationDate(e.target.value)}
+                  />
+                  <label>CVV</label>
+                  <input
+                    type="text"
+                    value={cvv}
+                    onChange={(e) => setCvv(e.target.value)}
+                  />
+                </fieldset>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          ) : (
+            <div>
+              <Link to="/">Home</Link>
+              <p>Log in first</p>
+            </div>
+          )}
         </div>
       )}
     </div>
