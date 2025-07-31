d342547f5346e43a7e47bfe8886a5565b5ef0d1f
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-07-31 12:34:27 +0300

    Show welcome message or login prompt on Home page based on user status

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
-
 function Home() {
+  const name = JSON.parse(localStorage.getItem("user"))?.name || "";
   return (
-    <ul>
-      <li>
-        <Link to="/register">Register</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/menu">Menu</Link>
-      </li>
-      <li>
-        <Link to="/my-reservations">User Page</Link>
-      </li>
-    </ul>
+    <div>
+      {name ? <p>Welcome {name}!</p> : <p>Hello, please log in to begin!</p>}
+      <ul>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/my-reservations">User Page</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 export default Home;
