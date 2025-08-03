468766452c423c6bbe574fcacd72ea87090a05bc
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-03 15:59:40 +0300

    Add deleteReservation controller using findByIdAndDelete

--- server/controllers/reservationController.js
+++ server/controllers/reservationController.js
@@ -57,7 +57,24 @@ const listReservations = async (req, res) => {
   }
 };
 
+const deleteReservation = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const reservation = await Reservation.findByIdAndDelete(id);
+    if (!reservation)
+      return res.status(404).json({ message: "No reservation found" });
+
+    return res.status(200).json({ message: "Success" });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      message: "An error occurred while trying to get reservations",
+    });
+  }
+};
+
 module.exports = {
   createReservation,
   listReservations,
+  deleteReservation,
 };
