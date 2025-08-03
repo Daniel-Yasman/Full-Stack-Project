6c6e04121cf749f580fb96f4c6fa15a78d613bcd
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-03 17:44:57 +0300

    Refactor SuccessModal to display formatted reservation time using Luxon

--- client/src/components/SuccessModal.jsx
+++ client/src/components/SuccessModal.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
+import { DateTime } from "luxon";
 
-function SuccessModal({ name, food, date, time }) {
+function SuccessModal({ name, food, time }) {
   return (
     <div className="border-1 w-[350px] h-[225px] flex flex-col items-center justify-center absolute z-10 bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Link to="/">X</Link>
       <h1>Huzzah!</h1>
       <p>Your order was successfull {name}!</p>
       <p>
-        At {time} on {date} your {food} will be waiting for you!
+        On
+        {DateTime.fromISO(time)
+          .setZone("Asia/Jerusalem")
+          .toFormat("MMMM d, yyyy - HH:mm")}{" "}
+        your {food} will be waiting for you!
       </p>
     </div>
   );
