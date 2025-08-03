e44400420a9ea0c77dec954000bd3c0cbbc85fc2
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-03 16:00:37 +0300

    Add DELETE /:id route for reservation cancellation

--- server/routes/reservationRoutes.js
+++ server/routes/reservationRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const {
   createReservation,
   listReservations,
+  deleteReservation,
 } = require("../controllers/reservationController");
 
 router.post("/", createReservation);
 router.get("/", listReservations);
-
+router.delete("/:id", deleteReservation);
 module.exports = router;
