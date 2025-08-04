d99f3838e9312ea8f4891943a58d43744d153531
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-04 18:46:21 +0300

    Add getCart controller to fetch user cart

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,6 +17,21 @@ async function addToCart(req, res) {
       .json({ message: "An error occurred while trying to add to cart" });
   }
 }
+
+async function getCart(req, res) {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    return res.status(200).json({ message: "Success", cart: user.cart });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while trying to fetch the cart" });
+  }
+}
 module.exports = {
   addToCart,
+  getCart,
 };
