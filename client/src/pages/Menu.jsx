bf3bba22665939af1a27379c6b8dafb7f7f09bbc
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-04 19:11:14 +0300

    Add Cart component to Menu for displaying item count

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Cart from "../components/Cart";
 import FoodList from "../components/FoodList";
 function Menu() {
   return (
     <div>
       <div>
         <Link to="/">Home</Link>
+        <Cart />
       </div>
       <FoodList />
     </div>
