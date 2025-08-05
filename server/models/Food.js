c76db3432e796e06cfad8067d3e0f96076551f8d
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-05 16:23:11 +0300

    Add price and image fields to Food schema

--- server/models/Food.js
+++ server/models/Food.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema({
   name: String,
+  price: Number,
+  image: String,
 });
 
 module.exports = mongoose.model("Food", foodSchema);
