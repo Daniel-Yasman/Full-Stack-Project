f037288c1c10f11e77b1a58e66ec63c1017a3819
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-05 20:00:14 +0300

    Refactor Cart to use CartContext instead of prop

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,8 +1,11 @@
-function Cart({ cartCount }) {
+import { useCart } from "../context/CartContext";
+function Cart() {
+  const { cartCount } = useCart();
   return (
     <div>
       <p>Cart ({cartCount})</p>
     </div>
   );
 }
+
 export default Cart;
