bfaaf56a9a2d0f7d67049a2035333ff603dcffe3
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-06 18:01:34 +0300

    Add DELETE route for removing specific item from user's cart

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,8 +4,10 @@ const {
   addToCart,
   getCart,
   updateCart,
+  removeItem,
 } = require("../controllers/userController");
 router.post("/:userId/cart", addToCart);
 router.get("/:userId/cart", getCart);
 router.patch("/:userId/cart", updateCart);
+router.delete("/:userId/cart/:foodId", removeItem);
 module.exports = router;
