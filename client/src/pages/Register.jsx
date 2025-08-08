a7558f836416cdf902d9d77338ee849e21f5a79b
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-08 17:04:57 +0300

    Redirect to login after successful registration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState(null);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -36,6 +38,7 @@ function Register() {
     setEmail("");
     setPassword("");
     setPhone("");
+    navigate("/login");
   };
 
   return (
