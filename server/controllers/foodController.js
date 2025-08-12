9d3a6bff534a5b72eca50ea8b54b7d85cf5ad29d
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-12 12:31:45 +0300

    return 200 with { foods } array instead of 404 or message wrapper

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -2,15 +2,11 @@ const Food = require("../models/Food");
 
 const listFoodItems = async (req, res) => {
   try {
-    const foods = await Food.find({});
-    if (foods.length === 0)
-      return res.status(404).json({ message: "No Foods found" });
-    return res.status(200).json({ message: "Success", foods });
-  } catch (error) {
-    console.error(error.message);
-    return res
-      .status(500)
-      .json({ message: "An error occurred while trying to list food items" });
+    const foods = await Food.find({}).lean();
+    return res.status(200).json({ foods });
+  } catch (err) {
+    if (process.env.NODE_ENV !== "production") console.error(err);
+    return res.status(500).json({ error: "internal_server_error" });
   }
 };
 module.exports = {
