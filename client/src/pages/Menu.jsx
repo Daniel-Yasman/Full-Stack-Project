5052e3ee18a6ff0f741006e9f38d96d8424f5620
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-08-13 12:34:30 +0300

    show cart link only when user is authenticated
    - Added useAuth import and destructuring to access user from AuthContext
    - Updated JSX to conditionally render the cart link only if user exists

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
+import { useAuth } from "../context/AuthContext";
 import Cart from "../components/Cart";
 import FoodList from "../components/FoodList";
 
 function Menu() {
   const { cartCount, fetchCart } = useCart();
+  const { user } = useAuth();
 
   return (
     <div>
       <div>
         <Link to="/">Home</Link>
-        <Link to="/cart">
-          <Cart cartCount={cartCount} />
-        </Link>
+        {user && (
+          <Link to="/cart">
+            <Cart cartCount={cartCount} />
+          </Link>
+        )}
       </div>
       <FoodList fetchCart={fetchCart} />
     </div>
