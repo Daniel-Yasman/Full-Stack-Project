945cc79f6826c704aaa8ec961e9b255314378fbe
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-09-20 15:14:28 +0300

    Add Subscriber model
    - email: required, unique, indexed
    - enable timestamps for createdAt/updatedAt

---
+++ server/models/Subscriber.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const subscriberSchema = new mongoose.Schema(
+  { email: { type: String, unique: true, required: true, index: true } },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Subscriber", subscriberSchema);
