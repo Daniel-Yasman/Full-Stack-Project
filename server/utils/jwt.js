e698ef4aff734c3ec3aec86f3eec5c7080f5ba7e
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-09-20 15:21:03 +0300

    Include role in JWT payload
    - sign: accept userId and role, embed both in token
    - log JWT_EXPIRES_IN env value for visibility

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-exports.sign = (userId) => {
-  return jwt.sign({ sub: userId }, process.env.JWT_SECRET, {
+exports.sign = (userId, role) => {
+  console.log("JWT_EXPIRES_IN:", process.env.JWT_EXPIRES_IN);
+
+  return jwt.sign({ sub: userId, role }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
