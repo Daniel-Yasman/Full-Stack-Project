5638ead9469592e74fbc0581702179d8c3252ce7
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-09-20 15:15:13 +0300

    Add role field to User schema
    - role: enum ['user','admin'], default 'user'

--- server/models/User.js
+++ server/models/User.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
       quantity: Number,
     },
   ],
+  role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
 module.exports = mongoose.model("User", userSchema);
